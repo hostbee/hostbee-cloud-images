d819809908eb9ec5d4519d8e0a31616adf6ff4e1
Akatsukiro <aka@bep.ink>
2024-11-15 21:59:01 +0800

    feat/centos7: 添加SSH相关脚本
    美化SSH登录后的输出；仅允许root用户登录

---
+++ http/beautify-ssh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat > /etc/motd <<EOF
+      :::    :::  ::::::::   :::::::: ::::::::::: :::::::::  :::::::::: :::::::::: 
+     :+:    :+: :+:    :+: :+:    :+:    :+:     :+:    :+: :+:        :+:         
+    +:+    +:+ +:+    +:+ +:+           +:+     +:+    +:+ +:+        +:+          
+   +#++:++#++ +#+    +:+ +#++:++#++    +#+     +#++:++#+  +#++:++#   +#++:++#      
+  +#+    +#+ +#+    +#+        +#+    +#+     +#+    +#+ +#+        +#+            
+ #+#    #+# #+#    #+# #+#    #+#    #+#     #+#    #+# #+#        #+#             
+###    ###  ########   ########     ###     #########  ########## ##########       
+
+Welcome! Have a great day!
+EOF
+
+echo "==> setting up root login"
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+echo "==> forbidding common users login"
+touch /etc/nologin
