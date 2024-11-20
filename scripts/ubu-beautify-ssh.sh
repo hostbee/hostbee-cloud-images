3e3c4a6e0212edd0e3ed7a4f752a464e5d409ff0
Akatsukiro <aka@bep.ink>
2024-11-20 21:05:22 +0800

    fix/ubuntu24: Ubuntu24使用ssh而非sshd

---
+++ scripts/ubu-beautify-ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat > /etc/motd <<EOF
+      :::    :::  ::::::::   :::::::: ::::::::::: :::::::::  :::::::::: :::::::::: 
+     :+:    :+: :+:    :+: :+:    :+:    :+:     :+:    :+: :+:        :+:         
+    +:+    +:+ +:+    +:+ +:+           +:+     +:+    +:+ +:+        +:+          
+   +#++:++#++ +#+    +:+ +#++:++#++    +#+     +#++:++#+  +#++:++#   +#++:++#      
+  +#+    +#+ +#+    +#+        +#+    +#+     +#+    +#+ +#+        +#+            
+ #+#    #+# #+#    #+# #+#    #+#    #+#     #+#    #+# #+#        #+#             
+###    ###  ########   ########     ###     #########  ########## ##########       
+
+Welcome! Have a great day!
+EOF
+
+echo "==> setting up root login"
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl restart ssh
