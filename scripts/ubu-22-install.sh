e1b30d20a42c1e4fd3a0ee47b0558885fc534832
Akatsukiro <aka@bep.ink>
2024-11-26 19:53:46 +0800

    feat/ubuntu22: 添加对Ubuntu22的支持

---
+++ scripts/ubu-22-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eux
+
+echo "==> waiting for cloud-init to finish"
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
+    echo 'Waiting for Cloud-Init...'
+    sleep 1
+done
+
+echo "==> change repo souces"
+if [ "$CN_FLAG" == "true" ]; then
+    echo "use CN sources"
+    sudo cat > /etc/apt/sources.list <<EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+
+# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+EOF
+
+else
+    echo "use default sources"
+fi
+
+echo "==> updating apt cache"
+sudo apt-get update -qq
+
+echo "==> upgrade apt packages"
+sudo apt-get upgrade -y -qq
+
+echo "==> installing qemu-guest-agent"
+sudo apt-get install -y -qq qemu-guest-agent
+
+echo "==> installing common packages"
+sudo apt-get install -y -qq curl wget git unzip vim
