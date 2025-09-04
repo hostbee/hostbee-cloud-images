7f58645ab81963a0a3c6ec4f69017cdd956225ff
sommio <sommio.mail@gmail.com>
2025-09-04 16:29:34 +0800

    feat(almalinux): Added configuration files and related scripts for AlmaLinux 10.

---
+++ scripts/almalinux-10-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eux
+
+echo "==> waiting for cloud-init to finish"
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
+    echo 'Waiting for Cloud-Init...'
+    sleep 1
+done
+
+echo "==> change repo souces"
+if [ "$CN_FLAG" == "true" ]; then
+    echo "use CN sources"
+    sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+        -e 's|^#\s*baseurl=https://repo.almalinux.org/almalinux|baseurl=https://mirror.nju.edu.cn/almalinux|g' \
+        -i.bak \
+        /etc/yum.repos.d/almalinux*.repo
+else
+    echo "use default sources"
+fi
+
+echo "==> updating yum cache"
+sudo yum makecache
+
+echo "==> upgrade yum packages"
+sudo yum update -y
+
+echo "==> installing common packages"
+sudo yum install -y qemu-guest-agent wget vim
